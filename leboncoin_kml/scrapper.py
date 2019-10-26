573935febffec7db2c192b37dd6212562aa63ae1
Xavier Tolza <tolza.xavier@gmail.com>
2019-10-26 11:33:14 +0200

    Implemented browser anonimisation

--- leboncoin_kml/scrapper.py
+++ leboncoin_kml/scrapper.py
@@ -1,11 +1,23 @@
+import re
+import subprocess
+
 from selenium import webdriver
 import numpy as np
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from user_agent import generate_user_agent
+
+
+class ProxyError(Exception):
+    pass
 
 
 class Firefox(webdriver.Firefox):
+    pref_types = {str: "String", int: "Int", bool: "Bool"}
+    ip_finder = re.compile(".+[^\d](\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,6}).+")
+
     def __init__(self, headless=False):
         options = Options()
         options.headless = headless
@@ -61,24 +73,33 @@ class Firefox(webdriver.Firefox):
 
     def set_preference(self, **elements):
         self.new_tab("about:config")
+
         try:
             script = """
             var prefs = Components.classes["@mozilla.org/preferences-service;1"]
                 .getService(Components.interfaces.nsIPrefBranch);
-            prefs.setBoolPref(arguments[0], arguments[1]);
+            prefs.set%sPref(arguments[0], arguments[1]);
             """
 
+            searchbar = None
             try:
                 self.find_element_by_id("warningButton").click()
+                searchbar = self.find_element_by_id("textbox").find_element_by_css_selector("input")
             finally:
                 for key, value in elements.items():
-                    self.find_element_by_id("textbox").find_element_by_css_selector("input").send_keys(key)
-                    self.execute_script(script, key, value)
+                    value_type = type(value)
+                    if searchbar is not None:
+                        searchbar.clear()
+                        searchbar.send_keys(key)
+                    self.execute_script(script % self.pref_types[value_type], key, value)
+        except KeyError:
+            raise ValueError(f"Wrong type for pref {key} value: {str(value_type)}. Supported types are "
+                             f"{list(self.pref_types.keys())}")
         finally:
             self.close_tab()
 
-    def set_proxy(self, type=1, **proxies):
-        pref = {"network.proxy.type": type}
+    def set_proxy(self, type=1, share_proxy_settings=False, **proxies):
+        pref = {"network.proxy.type": type, "network.proxy.share_proxy_settings": share_proxy_settings}
         for name, (addr, port) in proxies.items():
             pref.update({f"network.proxy.{name}": addr, f"network.proxy.{name}_port": port})
         self.set_preference(**pref)
@@ -89,18 +110,36 @@ class Firefox(webdriver.Firefox):
     def disable_js(self):
         self.set_preference(**{"javascript.enabled": False})
 
+    def get(self, url):
+        try:
+            super(Firefox, self).get(url)
+        except WebDriverException as e:
+            if "about:neterror?e=proxyConnectFailure" in str(e):
+                raise ProxyError("Proxy connexion refused. Are your proxy settings correct?")
+            raise
+
+    def set_user_agent(self, value):
+        self.set_preference(**{"general.useragent.overridepreference": value})
+
+    def generate_user_agent(self, **kwargs):
+        return generate_user_agent(**kwargs)
+
+    def generate_proxy(self):
+        cmd = "proxybroker find --types SOCKS5 --strict -l 1"
+        res = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE)
+        res = res.stdout
+        res = self.ip_finder.match(res.decode("utf-8")).groups()[0]
+        res = list(res.split(":"))
+        res[1] = int(res[1])
+        return res
+
+    def change_identity(self):
+        self.set_proxy(socks=self.generate_proxy())
+        self.set_user_agent(self.generate_user_agent())
+
 
 if __name__ == '__main__':
     with Firefox() as f:
-        f.new_tab("https://google.com")
-        f.new_tab("https://google.com")
-        f.new_tab("https://google.com")
-        print(f.tab)
-        f.tab = 0
-        print(f.tab)
-        f.tab = 1
-        f.close_tab()
-        f.disable_js()
-        f.enable_js()
-        f.set_proxy(sock=("localhost", 9095))
+        f.change_identity()
+        f.get("https://webkay.robinlinus.com/")
         pass
