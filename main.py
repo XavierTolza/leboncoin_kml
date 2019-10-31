c6a32ec156c9ea777bddd59cdbe9f87118020522
Xavier Tolza <tolza.xavier@gmail.com>
2019-10-31 07:24:46 +0100

    simplified main

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-from argparse import ArgumentParser
 from os import remove
 from os.path import isfile
 
 from leboncoin_kml.config import Config
-from leboncoin_kml.lbc import LBC, FinalPageReached
+from leboncoin_kml.lbc import LBC
 
 
-def main(headless=False):
+def main():
     conf = Config()
-    conf.headless = headless
 
     log_file = conf.log_file
     if log_file is not None and isfile(log_file):
         remove(log_file)
 
     d = LBC(conf)
-    try:
-        with d:
-            d.run()
-        print("finished")
-    except FinalPageReached:
-        print("Finished research")
-
-
-def parse():
-    parser = ArgumentParser()
-    parser.add_argument("--headless", action="store_true")
-    return parser.parse_args()
+    with d:
+        d.run()
+    print("finished execution")
 
 
 if __name__ == '__main__':
-    args = parse()
-
-    main(**args.__dict__)
+    main()
