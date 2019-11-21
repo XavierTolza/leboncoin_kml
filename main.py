101b18ddc05368f8d662f70396d6b05036912916
Xavier TOLZA <xto@ffly4u.com>
2019-11-21 13:24:21 +0100

    Amélioration du plantage

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from os import remove
 from os.path import isfile
 
 from leboncoin_kml.config import Config
-from leboncoin_kml.lbc import LBC, MaximumNumberOfFailures, read_file, LBCError
+from leboncoin_kml.lbc import LBC, read_file
 from leboncoin_kml.mail import Sender
 
 
@@ -23,26 +23,31 @@ def main():
             with d:
                 d.run()
             finished = True
-        except MaximumNumberOfFailures as e:
-            d.log.error("Restarting scrapper")
-            conf.url = e.last_url
-            previous_result = e.result
         except Exception as e:
+            d.log.error("Restarting scrapper")
             n_error += 1
             tb = traceback.format_exc()
-            d.log.critical(f"General error: {str(e)}\n{tb}")
-            conf.url = d.current_lbc_url
-            previous_result = d.result
+            d.log.critical(f"General error (counter={n_error}): \n{str(type(e))}: {str(e)}\n{tb}")
 
             if n_error > 10:
                 d.log.critical("Too many critical errors, stopping the bot")
                 finished = True
                 # Too many failures, sending the mail
                 sender = Sender(conf)
+
                 attachments = {"log.txt": read_file(conf.log_file)}
-                if issubclass(type(e), LBCError):
+                # noinspection PyBroadException
+                try:
+                    attachments.update(d.make_attachments(d.result))
+                except Exception:
+                    pass
+
+                if hasattr(e, "info"):
                     attachments.update(e.info)
                 sender(attachments, "Erreur robot LBC", "Ci joint des infos sur le plantage du robot")
+            else:
+                conf.url = d.current_lbc_url
+                previous_result = d.result
 
     print("finished execution")
 
