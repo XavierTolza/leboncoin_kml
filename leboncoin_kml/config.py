21f37a6489c51f1a968c048698c39be90cc7540d
Xavier Tolza <xto@ffly4u.com>
2020-01-21 18:17:12 +0100

    config guillaume

--- leboncoin_kml/config.py
+++ leboncoin_kml/config.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
-from logging import INFO
+from logging import DEBUG, INFO
+from .common import DEBUG as is_debugging
+from os.path import abspath, basename, join
 
 from attr import dataclass
 from numpy import inf
@@ -8,44 +10,48 @@ from .secret import *
 
 tomorrow = datetime.today() + timedelta(days=1)
 tomorrow_str = tomorrow.strftime("%Y-%m-%d")
+tomorrow_morning = datetime.strptime(tomorrow_str + " 9:00:00", '%Y-%m-%d %H:%M:%S').timestamp()
 
 
 @dataclass
 class Config(object):
-    log_level = INFO
+    log_level = DEBUG
     log_file = "log.txt"
-    url = "https://www.leboncoin.fr/recherche/?category=9&locations=r_16&" \
-          "real_estate_type=1&immo_sell_type=old,new&price=min-325000&square=60-max"
+    url = "https://www.leboncoin.fr/recherche/?category=9&locations=Pau_64000__43.2965_-0.37432_7305_30000&" \
+          "real_estate_type=1&price=75000-200000&rooms=3-max&square=70-max"
     scrap_time = 25  # hours
     maximum_number_retry = 20
     loading_timeout = 5  # sec
-    headless = True
-    output_folder = "/tmp/output"
-    start_anonymously = headless
+    headless = not is_debugging
+    output_folder = join(basename(abspath(__file__)), "../../bdd_lbc_gui")
+    start_anonymously = True
     use_proxy = start_anonymously
+    solve_captcha_by_hand = not use_proxy
     google_maps_api_key = google_maps_api_key
     openroute_api_key = openroute_api_key
     directions = dict(
-        boulot_velo=(inf, dict(
-            destination=work,
-            mode="bicycling",
-            arrival_time=datetime.strptime(tomorrow_str + " 9:00:00", '%Y-%m-%d %H:%M:%S').timestamp()
-        )),
-        boulot_voiture=(30, dict(
+        # boulot_velo=(inf, dict(
+        #     destination=work,
+        #     mode="bicycling",
+        #     arrival_time=tomorrow_morning
+        # )),
+        boulot_voiture=(inf, dict(
             destination=work,
             mode="driving",
-            arrival_time=datetime.strptime(tomorrow_str + " 9:00:00", '%Y-%m-%d %H:%M:%S').timestamp()
-        ))
-
+            arrival_time=tomorrow_morning
+        )),
+        Pau=(inf, dict(
+            destination="Pau 31400",
+            mode="driving",
+            arrival_time=tomorrow_morning
+        )),
     )
     filters = [
-        lambda x: (x.latlng[0] < 43.597221 and x.latlng[0] > 43.444315
-                   and x.latlng[1] > 1.350069
-                   and not x.a_construire)
+        lambda x: (not x.a_construire)
     ]
     email_receivers = email_dest
     email_sender = email_sender
     email_password = email_password
     encoding = "utf-8"
     skip_elements_already_in_bdd = False
-    mapbox_token=mapbox_token
+    mapbox_token = mapbox_token
